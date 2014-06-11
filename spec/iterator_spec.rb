1499890abdeb2032b7a2c664d5b6d5f2f67ad04b
wfjeff <wm.jeffries@gmail.com>
2014-06-11 10:02:11 -0400

    add test case for empty iterators

--- spec/iterator_spec.rb
+++ spec/iterator_spec.rb
@@ -2,11 +2,16 @@ require './iterator'
 
 describe Iterator do 
   let(:iterator){Iterator.new([1,2])}
+  let(:null_iterator){Iterator.new([])}
   context "#next" do 
     it 'returns the item and moves next' do
       expect(iterator.next).to eq 1
       expect(iterator.next).to eq 2
       expect(iterator.next).to eq nil
+    end
+
+    it "works on empty iterators" do
+      expect(null_iterator.next).to eq nil
     end
   end
 
@@ -18,6 +23,10 @@ describe Iterator do
       iterator.next
       iterator.next
       expect(iterator.has_next?).to eq false
+    end
+
+    it 'returns false for empty iterators' do 
+      expect(null_iterator.has_next?).to eq false
     end
   end
 end
